722ef3383b53f3b36639da81477fb4cb7af5c5fb
zoulvzhou <940933073@qq.com>
2018-05-03 00:24:58 +0800

    Spring4之Hello  world

---
+++ src/main/java/com/zou/service/Test.java
@@ -0,0 +1,23 @@
+package com.zou.service;
+
+import com.zou.test.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-configuration.xml");
+
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloworld");
+
+        helloWorld.say();
+
+
+    }
+
+
+}
---
+++ src/main/java/com/zou/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.zou.test;
+
+public class HelloWorld {
+
+
+    public void say() {
+        System.out.println("Hello  Spring");
+    }
+
+}
